b8b215c8822418d6ecc3babb120ea2993f390e8d
Gabriel Desktop <gabriel.goldstein@cix.csi.cuny.edu>
2019-12-16 08:32:51 -0500

    First Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from './api.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'TestAPICall';
+
+  emojiArray: any = [];
+
+  constructor(private api: ApiService) { }
+
+  getEmojiArray() {
+    this.api.getApiCallURL().subscribe(data => {
+      for (const d of (data as any)) {
+        this.emojiArray.push({
+          label: d.label,
+          emoji: d.emoji
+        });
+      }
+      console.log(this.emojiArray);
+    })
+  }
+
 }
